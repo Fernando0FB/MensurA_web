3e269f2c9cb4f83cf11d5a9518fb6d041ec0cb5c
Fernando.FB <FernandoFBonetti@hotmail.com>
2025-10-30 13:27:14 -0300

    Removendo o valor de idade do paciente.
    Caso necessário puxar idade, deve-se levar em consideração com base na data de nascimento.
    
    /2

--- src/main/java/com/MensurA/web/features/mensuracoes/dto/analises/PacienteAnaliseDTO.java
+++ src/main/java/com/MensurA/web/features/mensuracoes/dto/analises/PacienteAnaliseDTO.java
@@ -3,8 +3,8 @@ package com.MensurA.web.features.mensuracoes.dto.analises;
 import com.MensurA.web.commom.enums.Sexo;
 import com.MensurA.web.features.pacientes.model.Paciente;
 
-public record PacienteAnaliseDTO(String nome, Integer idade, Sexo sexo, String cpf) {
+public record PacienteAnaliseDTO(String nome, Sexo sexo, String cpf) {
     public static PacienteAnaliseDTO from(Paciente paciente) {
-        return new PacienteAnaliseDTO(paciente.getNome(), paciente.getIdade(), paciente.getSexo(), paciente.getCpf());
+        return new PacienteAnaliseDTO(paciente.getNome(), paciente.getSexo(), paciente.getCpf());
     }
 }
