79380b29757f1058b6e0ea0eb46bf8c62ed8a07c
Fernando.FB <FernandoFBonetti@hotmail.com>
2025-11-07 18:57:47 -0300

    Adicionando DataHora no analiseDTO

--- src/main/java/com/MensurA/web/features/mensuracoes/dto/analises/AvaliacaoAnaliseDTO.java
+++ src/main/java/com/MensurA/web/features/mensuracoes/dto/analises/AvaliacaoAnaliseDTO.java
@@ -5,6 +5,8 @@ import com.MensurA.web.commom.enums.Lado;
 import com.MensurA.web.commom.enums.Movimento;
 import com.MensurA.web.features.mensuracoes.model.Mensuracao;
 
+import java.time.LocalDateTime;
+
 public record AvaliacaoAnaliseDTO(
         Articulacao articulacao,
         Lado lado,
@@ -14,10 +16,11 @@ public record AvaliacaoAnaliseDTO(
         Integer anguloFinal,
         Integer excursao,
         Integer dor,
+        LocalDateTime dataHora,
         String observacao
 ) {
     public static AvaliacaoAnaliseDTO from(Mensuracao mensuracao) {
         return new AvaliacaoAnaliseDTO(mensuracao.getArticulacao(), mensuracao.getLado(), mensuracao.getMovimento(), mensuracao.getPosicao(),
-                mensuracao.getAnguloInicial(), mensuracao.getAnguloFinal(), mensuracao.getExcursao(), mensuracao.getDor(), mensuracao.getObservacoes());
+                mensuracao.getAnguloInicial(), mensuracao.getAnguloFinal(), mensuracao.getExcursao(), mensuracao.getDor(), mensuracao.getDataHora(), mensuracao.getObservacoes());
     }
 }
